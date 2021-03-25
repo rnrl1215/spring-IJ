67aceb8e3fac4b935103745e7c9e12cd5b5cc175
skahn1215 <d1215@naver.com>
2021-03-26 00:07:15 +0900

    회원관리 시스템 기능 구현 및 테스트 케이스 작성

--- src/test/java/com/example/hello/spring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/example/hello/spring/repository/MemoryMemberRepositoryTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.*;
 public class MemoryMemberRepositoryTest {
     MemoryMemberRepository repository = new MemoryMemberRepository();
 
-    @AfterEach
+    @AfterEach // call back
     public void afterEach()
     {
         repository.clearStore();
