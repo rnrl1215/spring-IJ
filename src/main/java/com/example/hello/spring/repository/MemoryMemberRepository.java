0c9e521289ec27f0856b91f106dbcb421d7e2ac5
skahn1215 <d1215@naver.com>
2021-03-26 00:04:46 +0900

    회원관리 시스템 기능 구현 및 테스트 케이스 작성

--- src/main/java/com/example/hello/spring/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/hello/spring/repository/MemoryMemberRepository.java
@@ -4,14 +4,14 @@ import com.example.hello.spring.domain.Member;
 
 import java.util.*;
 
-public class MemoerMemberRepository implements MemberRpository {
+public class MemoryMemberRepository implements MemberRpository {
 
     private static Map<Long, Member> store = new HashMap<>();
-    private static long squence = 0L;
+    private static long sequence = 0L;
 
     @Override
     public Member save(Member member) {
-        member.setId(++squence);
+        member.setId(++sequence);
         store.put(member.getId(), member);
         return null;
     }
@@ -31,5 +31,10 @@ public class MemoerMemberRepository implements MemberRpository {
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore()
+    {
+        store.clear();
     }
 }
