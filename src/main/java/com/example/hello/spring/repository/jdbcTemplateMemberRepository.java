d1ec7194563f0bf6d69878c0480e9a65c301d937
skahn1215 <d1215@naver.com>
2021-03-29 23:08:48 +0900

    변경사항 1. simple jdbc 이용

---
+++ src/main/java/com/example/hello/spring/repository/jdbcTemplateMemberRepository.java
@@ -0,0 +1,80 @@
+package com.example.hello.spring.repository;
+
+import com.example.hello.spring.domain.Member;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class jdbcTemplateMemberRepository implements MemberRepository {
+
+    private  final JdbcTemplate jdbcTemplate;
+
+    // 생성자가 하나일때는 @Autowired 생략가능
+    public jdbcTemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    
+    @Override
+    public Member save(Member member) {
+        // Table과 Id를 알면 insert 할수 있다.
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?",memberRowMapper());
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+
+    /* 람다로 변경전
+    private RowMapper<Member> memberRowMapper() {
+        return new RowMapper<Member>() {
+            @Override
+            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return member;
+            }
+        };
+    }
+     */
+
+    // 람다로 변경함
+    // memberRowMapper를 이용히여 member를 리턴
+    private RowMapper<Member> memberRowMapper() {
+        // ramda
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
+    }
+}
